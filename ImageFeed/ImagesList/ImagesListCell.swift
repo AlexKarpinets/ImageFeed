76ad1c3ffc65fe3522ab6f6ba2764c499338c5c3
Alexander Karpinets <alexanderkarpinets@gmail.com>
2023-03-23 19:31:48 +0300

    Complete sprint_08

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -1,11 +1,10 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-  
+    
     @IBOutlet weak var images: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var favoriteButton: UIButton!
     
     static let reuseIdentifier = "ImagesListCell"
-    
 }
