466cfb0f5657b0cc31682e3618a7d1600bfe23e2
Alexander Karpinets <alexanderkarpinets@gmail.com>
2023-04-03 19:46:33 +0300

    Add SingleImageViewController

---
+++ ImageFeed/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    @IBOutlet private weak var profileImage: UIImageView!
+    @IBOutlet private weak var profileSurname: UILabel!
+    @IBOutlet private weak var profileEmail: UILabel!
+    @IBOutlet private weak var profileText: UILabel!
+    @IBOutlet private weak var profileLogOut: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
