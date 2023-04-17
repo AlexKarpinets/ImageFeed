1714a78b83d7e9019e4d58336a3cb000267b043d
Alexander Karpinets <alexanderkarpinets@gmail.com>
2023-04-17 19:01:35 +0300

    Add Constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants: String {
+    case accessKey = "UWHoiK56oeXkMMFakelxYjtr9SXBJac9RTSwG-K-rTc"
+    case secretKey = "c0jHT2QfQFZt-elMrx5JYbzcKO4LwCSZ4thWcvepqoo"
+    case redirectURl = "urn:ietf:wg:oauth:2.0:oob"
+    case accessScope = "public+read_user+write_likes"
+    case defaultBaseURl = "https://api.unsplash.com"
+}
