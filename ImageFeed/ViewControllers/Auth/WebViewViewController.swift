0f73f37293c925d159dacccf7a37d8a934e825f8
Alexander Karpinets <alexanderkarpinets@gmail.com>
2023-04-23 14:23:38 +0300

    Add ProgressView & KVO

--- ImageFeed/ViewControllers/Auth/WebViewViewController.swift
+++ ImageFeed/ViewControllers/Auth/WebViewViewController.swift
@@ -8,7 +8,8 @@ protocol WebViewViewControllerDelegate: AnyObject {
 
 final class WebViewViewController: UIViewController {
     
-    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak private var progressView: UIProgressView!
+    @IBOutlet weak private var webView: WKWebView!
     
     fileprivate let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
     
@@ -30,6 +31,33 @@ final class WebViewViewController: UIViewController {
         webView.load(request)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    override func observeValue(
+        forKeyPath keyPath: String?,
+        of object: Any?,
+        change: [NSKeyValueChangeKey : Any]?,
+        context: UnsafeMutableRawPointer?
+    ) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            updateProgress()
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+
+    private func updateProgress() {
+        progressView.progress = Float(webView.estimatedProgress)
+        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
+    }
+    
+    
     private func code(from navigationAction: WKNavigationAction) -> String? {
         if
             let url = navigationAction.request.url,
@@ -44,7 +72,7 @@ final class WebViewViewController: UIViewController {
         }
     }
     
-    @IBAction func didTapBackButton(_ sender: UIButton) {
+    @IBAction private func didTapBackButton(_ sender: UIButton) {
         delegate?.webViewViewControllerDidCancel(_vc: self)
     }
 }
@@ -56,7 +84,6 @@ extension WebViewViewController: WKNavigationDelegate {
         decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
     ) {
         if let code = code(from: navigationAction) {
-            
             decisionHandler(.cancel)
         } else {
             decisionHandler(.allow)
