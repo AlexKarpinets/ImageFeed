40cd0a8cf5a0689361ee2f15ab0c32b9627ca24e
Alexander Karpinets <alexanderkarpinets@gmail.com>
2023-05-17 21:31:31 +0300

    Add ImagesListService & LikeButton UI, Nettwork

--- ImageFeed/Services/ImagesListService.swift
+++ ImageFeed/Services/ImagesListService.swift
@@ -1,5 +1,51 @@
 import Foundation
 
+struct LikePhotoResult: Decodable {
+    let photo: PhotoResult?
+}
+
+struct Photo {
+    let id: String
+    let width: CGFloat
+    let height: CGFloat
+    let createdAt: Date?
+    let welcomeDescription: String?
+    let thumbImageURL: String?
+    let largeImageURL: String?
+    let isLiked: Bool
+}
+
+struct PhotoResult: Decodable {
+    let id: String
+    let createdAt: String?
+    let welcomeDescription: String?
+    let isLiked: Bool?
+    let urls: UrlsResult?
+    let width: CGFloat
+    let height: CGFloat
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case createdAt = "created_at"
+        case welcomeDescription = "description"
+        case isLiked = "liked_by_user"
+        case urls = "urls"
+        case width = "width"
+        case height = "height"
+    }
+}
+
+struct UrlsResult: Decodable {
+    let thumbImageURL: String?
+    let largeImageURL: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case thumbImageURL = "thumb"
+        case largeImageURL = "full"
+    }
+}
+
+
 final class ImagesListService {
     private (set) var photos: [Photo] = []
     private var lastLoadedPage: Int?
@@ -7,6 +53,8 @@ final class ImagesListService {
     private var task: URLSessionTask?
     static let DidChangeNotification = Notification.Name(rawValue: "ImagesListServiceDidChange")
     static let shared = ImagesListService()
+    private let storageToken = OAuth2TokenStorage()
+    private let dateFormatter = ISO8601DateFormatter()
     
     func fetchPhotosNextPage() {
         assert(Thread.isMainThread)
@@ -56,54 +104,94 @@ final class ImagesListService {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        let date = dateFormatter.date(from: photoResult.createdAt ?? "")
+        let _ = dateFormatter.date(from: photoResult.createdAt ?? "")
         
         return Photo.init(id: photoResult.id,
-                          size: CGSize(width: photoResult.width ?? 0, height: photoResult.height ?? 0),
-                          createdAt: date,
+                          width: CGFloat(photoResult.width),
+                          height: CGFloat(photoResult.height),
+                          createdAt: self.dateFormatter.date(from:photoResult.createdAt ?? ""),
                           welcomeDescription: photoResult.welcomeDescription,
                           thumbImageURL: photoResult.urls?.thumbImageURL,
                           largeImageURL: photoResult.urls?.largeImageURL,
                           isLiked: photoResult.isLiked ?? false)
     }
-}
-
-struct Photo {
-    let id: String
-    let size: CGSize
-    let createdAt: Date?
-    let welcomeDescription: String?
-    let thumbImageURL: String?
-    let largeImageURL: String?
-    let isLiked: Bool
-}
-
-struct PhotoResult: Decodable {
-    let id: String
-    let createdAt: String?
-    let welcomeDescription: String?
-    let isLiked: Bool?
-    let urls: UrlsResult?
-    let width: Int?
-    let height: Int?
     
-    enum CodingKeys: String, CodingKey {
-        case id = "id"
-        case createdAt = "created_at"
-        case welcomeDescription = "description"
-        case isLiked = "liked_by_user"
-        case urls = "urls"
-        case width = "width"
-        case height = "height"
+    func updatePhotos(_ photos: [Photo]) {
+        self.photos = photos
     }
     
-    struct UrlsResult: Decodable {
-        let thumbImageURL: String?
-        let largeImageURL: String?
+    func clean() {
+        photos = []
+        lastLoadedPage = nil
+        task?.cancel()
+        task = nil
+    }
+    
+    func changeLike(photoId: String, isLike: Bool, _ completion: @escaping (Result<Void, Error>) -> Void) {
+        assert(Thread.isMainThread)
+        task?.cancel()
         
-        enum CodingKeys: String, CodingKey {
-            case thumbImageURL = "thumb"
-            case largeImageURL = "full"
+        guard let token = storageToken.token else { return }
+        var request: URLRequest?
+        if isLike {
+            request = deleteLikeRequest(token, photoId: photoId)
+        } else {
+            request = postLikeRequest(token, photoId: photoId)
         }
+        guard let request = request else { return }
+        let session = URLSession.shared
+        let task = session.objectTask(for: request) { [weak self] (result: Result<LikePhotoResult, Error>) in
+            guard let self = self else { return }
+            self.task = nil
+            switch result {
+            case .success(let photoResult):
+                let isLiked = photoResult.photo?.isLiked ?? false
+                if let index = self.photos.firstIndex(where: { $0.id == photoResult.photo?.id }) {
+                    let photo = self.photos[index]
+                    let newPhoto = Photo(
+                        id: photo.id,
+                        width: photo.width,
+                        height: photo.height,
+                        createdAt: photo.createdAt,
+                        welcomeDescription: photo.welcomeDescription,
+                        thumbImageURL: photo.thumbImageURL,
+                        largeImageURL: photo.largeImageURL,
+                        isLiked: isLiked
+                    )
+                    self.photos = self.photos.withReplaced(itemAt: index, newValue: newPhoto)
+                }
+                completion(.success(()))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        self.task = task
+        task.resume()
+    }
+    
+    private func postLikeRequest(_ token: String, photoId: String) -> URLRequest? {
+        var requestPost = URLRequest.makeHTTPRequest(
+            path: "photos/\(photoId)/like",
+            httpMethod: "POST",
+            baseURL: URL(string: "\(Constants.defaultBaseURl)")!)
+        requestPost.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        return requestPost
+    }
+    
+    private func deleteLikeRequest(_ token: String, photoId: String) -> URLRequest? {
+        var requestDelete = URLRequest.makeHTTPRequest(
+            path: "photos/\(photoId)/like",
+            httpMethod: "DELETE",
+            baseURL: URL(string: "\(Constants.defaultBaseURl)")!)
+        requestDelete.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        return requestDelete
+    }
+}
+
+extension Array {
+    func withReplaced(itemAt: Int, newValue: Photo) -> [Photo] {
+        var photos = ImagesListService.shared.photos
+        photos.replaceSubrange(itemAt...itemAt, with: [newValue])
+        return photos
     }
 }
--- ImageFeed/ViewControllers/SingleImageViewController.swift
+++ ImageFeed/ViewControllers/SingleImageViewController.swift
@@ -1,3 +1,4 @@
+import Kingfisher
 import UIKit
 
 final class SingleImageViewController: UIViewController {
@@ -5,20 +6,20 @@ final class SingleImageViewController: UIViewController {
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var scrollView: UIScrollView!
     
-    var image: UIImage! {
+    private let alert = AlertPresenter()
+    
+    var imageURL: URL! {
         didSet {
             guard isViewLoaded else {return}
-            imageView.image = image
-            rescaleAndCenterImageInScrollView(image: image)
+            setImage()
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = image
-        rescaleAndCenterImageInScrollView(image: image)
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
+        setImage()
     }
     
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
@@ -38,8 +39,27 @@ final class SingleImageViewController: UIViewController {
         scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
     }
     
+    private func setImage() {
+        UIBlockingProgressHUD.show()
+        imageView.kf.setImage(with: imageURL) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let imageResult):
+                self.rescaleAndCenterImageInScrollView(image: imageResult.image)
+            case .failure:
+                alert.showAlert(in: self, with: AlertModel(
+                    title: "Ошибка",
+                    message: "Невозмоюно загрузить",
+                    buttonText: "OK",
+                    completion: nil),
+                                erorr: nil)
+            }
+            UIBlockingProgressHUD.dismiss()
+        }
+    }
+    
     @IBAction private func didTapShareButton(_ sender: UIButton) {
-        let activityItem = [image]
+        let activityItem = [imageView.image]
         let avc = UIActivityViewController(activityItems: activityItem as [AnyObject], applicationActivities: nil)
         present(avc, animated: true)
     }
@@ -52,5 +72,11 @@ final class SingleImageViewController: UIViewController {
 extension SingleImageViewController: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0)
+        let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0)
+        scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: 0, right: 0)
     }
 }
